26de3a36d7c2f5fcc0e40f55d97820feee35a72a
LoiTran <tranloi781@gmail.com>
2022-05-18 12:24:10 +0700

    general idea of create a password

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,36 @@
 // Assignment code here
 
 
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  var pass = window.prompt ('length of passwords');
+   pass = parseInt(pass);
+  
+  if (pass >=8){
+  window.confirm ('uppercase');
+    //create Upper case
+    var upper ="QWERTYUIOPASDFGHJKLZXCVBNM"
+  window.confirm ('lowercase');
+    //create Lower case
+    var lower = "qwertyuiopasdfghjklzxcvbnm"
+  window.confirm ('numeric');
+    //create randomNumber
+    var randomnumber =  Math.floor(Math.random()*10);
+  
+
+  window.confirm ('special characters');
+  
+  }
+
+  else {window.alert('something is wrong, please do it again')};
+  
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
