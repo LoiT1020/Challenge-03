741ec4b68ea57ff4b2d1a99589ba4d4f7d85a037
LoiTran <tranloi781@gmail.com>
2022-05-18 13:15:00 +0700

    follow template

--- Develop/script.js
+++ Develop/script.js
@@ -8,35 +8,51 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var chars="";
 
+  var begin="";
+ 
   var pass = window.prompt ('length of passwords');
-   pass = parseInt(pass);
-  
-  if (pass >=8){
-  window.confirm ('uppercase');
-    //create Upper case
-    var upper ="QWERTYUIOPASDFGHJKLZXCVBNM"
-  window.confirm ('lowercase');
-    //create Lower case
-    var lower = "qwertyuiopasdfghjklzxcvbnm"
-  window.confirm ('numeric');
-    //create randomNumber
-    var randomnumber =  Math.floor(Math.random()*10);
-  
+  pass = parseInt(pass);
+ 
+ if (pass >=8){
+ var confirmU = window.confirm ('uppercase');
+  if(confirmU){
+   //create Upper case
+   var upper ="QWERTYUIOPASDFGHJKLZXCVBNM"
+   source = upper;
+   
+ /*window.confirm ('lowercase');
+   //create Lower case
+   var lower = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+ window.confirm ('numeric');
+   //create randomNumber
+   var number ="0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+ window.confirm ('special characters');
+   var spec = "!@#$%^&*()0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";*/
 
-  window.confirm ('special characters');
-  
-  }
+for (var i=0;i<=pass;i++){
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  begin += chars.substring(randomNumber, randomNumber +1);
 
-  else {window.alert('something is wrong, please do it again')};
-  
-
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  var wheretoshow = document.querySelector(".card-body")
+  passwordText.value = begin;
+ 
 }
+  return true;
+ }
+   }
+
+
+
+ else {window.alert('something is wrong, please do it again')};
+ 
+}  
+
+  
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
